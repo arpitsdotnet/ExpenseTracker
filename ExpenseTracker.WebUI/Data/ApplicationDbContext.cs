40e11c29e8a1ed4485e5e4a579fe90fe7bd1b0bc
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2024-09-27 13:37:11 +0530

    Add project files.

---
+++ ExpenseTracker.WebUI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTracker.WebUI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.WebUI.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }  
+    public DbSet<Transaction> Transactions { get; set; }
+}
---
+++ ExpenseTracker.WebUI/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ExpenseTracker.WebUI.Models;
+
+public class Transaction
+{
+    [Key]
+    public int TransactionId { get; set; }
+
+    public int CategoryId { get; set; }
+
+    [ForeignKey(nameof(CategoryId))]
+    [ValidateNever]
+    public Category Category { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string? Note { get; set; }
+
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+}
